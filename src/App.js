ca9ae135d707fa71b32892b4e4e8e94b8bb95d0e
kzeimpekis <kzeimpekis@gmail.com>
2022-03-08 14:23:13 +0000

    display different topic when the user selects it

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import './App.css';
 import { BrowserRouter, Route, Routes, Navigate} from 'react-router-dom';
 import Header from './components/Header';
 import Articles from './components/Articles';
+import TopicsList from './components/TopicsList';
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
       <Header />
+      <TopicsList />
       <Routes>
         <Route path='/' element={<Navigate to='/articles' />}/>
         <Route path='/articles' element={<Articles />}/>
+        <Route path='/topics/:topic_slug' element={<Articles />}/>
       </Routes>
     </div>
    </BrowserRouter>
